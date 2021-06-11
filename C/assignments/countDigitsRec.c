948b520e691e437665940ee376cbf4b9a08743ab
Akash Dip <77287683+kod-it@users.noreply.github.com>
2021-06-11 13:13:43 +0530

    Assignment_6

---
+++ C/assignments/countDigitsRec.c
@@ -0,0 +1,26 @@
+ //Write  a  program  in  C  to  count  the  digits  of  a  givennumber using recursion. 
+#include <stdio.h>
+//function to count digits
+int countDigits(int num)
+{
+    static int count=0;
+    if(num>0)
+    {
+        count++;
+        countDigits(num/10);
+    }
+    else
+    {
+        return count;
+    }
+}
+int main()
+{
+    int number;
+    int count=0;
+    printf("Enter a positive integer number: ");
+    scanf("%d",&number);
+    count=countDigits(number);
+    printf("Total digits in number %d is: %d\n",number,count);
+    return 0;
+}
